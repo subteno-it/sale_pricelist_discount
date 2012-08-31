1b7f1cd618d94b6c3d295a442924a3c664cbd463
Benoit MOTTIN <benoit.mottin@syleam.fr>
2012-08-31 09:58:10 +0200

    [IMP] begin migration of the module from 5.0 to 6.1

--- __init__.py
+++ __init__.py
@@ -2,27 +2,26 @@
 ################################################################
 #
 # sale_pricelist_discount module for OpenERP, Sale pricelist discount
-# Copyright (C) 2009 SYLEAM Info Services (<http://www.Syleam.fr/>)
-#                 {PROGRAMMER'S NAME AND E.MAIL ADDRESS GOES HERE
-#                 THERE IS AS MANY LINE AS DEVELOPPERS INVOLVED}
+# Copyright (C) 2009-20112 SYLEAM Info Services (<http://www.Syleam.fr/>)
 #
 # This file is a part of sale_pricelist_discount
 #
 # sale_pricelist_discount is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # sale_pricelist_discount is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
 # GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ################################################################
 
-import object
+import sale_order_line
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
